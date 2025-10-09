729f736c736150be683d98c826d0bea4f2ff8aaf
Roberto <100007851.joan23@fje.edu>
2025-10-09 21:30:13 +0200

    color palette

---
+++ js/form.js
@@ -0,0 +1,21 @@
+(function(){
+  emailjs.init("GGgW7YvbJbWLGZql5"); 
+})();
+
+function enviarCorreo(e) {
+  e.preventDefault();
+
+  const params = {
+    name:  document.getElementById("name").value,   
+    email: document.getElementById("email").value,  
+    message: document.getElementById("message").value,
+    reply_to: document.getElementById("email").value
+  };
+
+  emailjs.send("service_kqu4jfl", "template_s0t71mq", params)
+    .then(() => {
+      document.getElementById("form-messages").innerHTML = "Correo enviado correctamente";
+      document.getElementById("contact-form").reset();
+    })
+    .catch(err => document.getElementById("form-messages").innerHTML = "Error al enviar: " + JSON.stringify(err));
+}
